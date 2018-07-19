8ffb876f23db59ae30a609e103969c30bb148c39
Heshie Brody <4hbrody@gmail.com>
2018-07-19 18:20:19 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,10 @@ end
 end
 
 def current_player(board)
-player = turn_count(board)
-  if player % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+player = turn_count(board) % 2 == 0 ? "X" : "O"
+  # if player % 2 == 0
+  #   return "X"
+  # else
+  #   return "O"
+  # end
 end
